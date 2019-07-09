94ba8ae4378f619ab004234e567f87fe7dfbe88a
Freddie <f.varnals@gmail.com>
2019-07-09 16:48:40 +0100

    Added form for player names

--- app.rb
+++ app.rb
@@ -4,9 +4,15 @@ set  :session_secret,"super secret"
 
 class Battle < Sinatra::Base
 	get '/' do
-		'Testing infrastructure working!'
+		erb(:index)
 	end
-	
+
+	post '/battle' do
+		@player_1_name = params[:player_1_name]
+		@player_2_name = params[:player_2_name]
+		erb(:intro)
+	end
+
 	# start the server if ruby file executed directly
 	run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,14 @@
-feature 'testing infrastructure' do
-  scenario 'Checks home page and returns text' do
+# feature 'testing infrastructure' do
+  # scenario 'Checks home page and returns text' do
+  #   visit('/')
+  #   expect(page).to have_content "Testing infrastructure working!"
+  # end
+feature 'Enter Names' do
+  scenario 'Submits names in form and prints to screen' do
     visit('/')
-    expect(page).to have_content "Testing infrastructure working!"
+    fill_in :player_1_name, with: 'Ben'
+    fill_in :player_2_name, with: 'Freddie'
+    click_button 'Submit'
+    expect(page).to have_content "Ben vs. Freddie"
   end
 end
