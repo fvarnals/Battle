a4a6ba6103c651c3cc1d2ec501980ae8bd32143b
Freddie <f.varnals@gmail.com>
2019-07-10 14:59:06 +0100

    Added sign_in_and_play function for RSpec, to keep the code DRY

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,20 +6,14 @@
 feature 'Enter Names' do
   let(:name) { double :name }
   scenario 'Submits names in form and prints to screen' do
-    visit('/')
-    fill_in :player_1_name, with: "Ben"
-    fill_in :player_2_name, with: 'Freddie'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Ben vs. Freddie"
   end
 end
 
 feature 'Display Hit Points' do
   scenario 'Displays player hit points on the screen during game' do
-    visit('/')
-    fill_in :player_1_name, with: "Man"
-    fill_in :player_2_name, with: "Car"
-    click_button 'Submit'
-    expect(page).to have_content "Car = 100HP"
+    sign_in_and_play
+    expect(page).to have_content "Freddie = 100HP"
   end
 end
